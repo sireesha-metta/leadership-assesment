549b92a6142a5a957936c5bf0ef92378e27deda9
sireesha-metta <sireesha.metta@Solventek.com>
2026-07-01 21:06:00 +0530

    added additional condition for db

--- config/db.js
+++ config/db.js
@@ -5,6 +5,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+   ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
 });
