de6209543c5d2567be7fbb9c901bdc1f5051a44c
sireesha-metta <sireesha.metta@Solventek.com>
2026-07-14 11:17:54 +0530

    added database related info

---
+++ database/Leadership_assessment_DB.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE  IF NOT EXISTS `leadership_assesment` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `leadership_assesment`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com    Database: leadership_assesment
+-- ------------------------------------------------------
+-- Server version	8.0.11-TiDB-v8.5.3-serverless
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `assessment_drafts`
+--
+
+DROP TABLE IF EXISTS `assessment_drafts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assessment_drafts` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `respondent_id` bigint unsigned NOT NULL,
+  `assessment_type` varchar(100) COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'leadership_reset',
+  `respondent_name` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `answered_count` int NOT NULL DEFAULT '0',
+  `draft_payload` json NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  UNIQUE KEY `uq_assessment_drafts_user_type` (`respondent_id`,`assessment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=2159721;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `assessment_submissions`
+--
+
+DROP TABLE IF EXISTS `assessment_submissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `assessment_submissions` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `respondent_id` bigint unsigned NOT NULL,
+  `assessment_type` varchar(80) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `respondent_name` varchar(255) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mobile` varchar(32) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `submitted_at` datetime DEFAULT NULL,
+  `total_score` decimal(12,2) NOT NULL DEFAULT '0',
+  `total_weighted_score` decimal(12,2) NOT NULL DEFAULT '0',
+  `submission_payload` longtext COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  UNIQUE KEY `uq_submission_once` (`respondent_id`,`assessment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=120001;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `file_upload_history`
+--
+
+DROP TABLE IF EXISTS `file_upload_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `file_upload_history` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `original_file_name` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `uploaded_by` int NOT NULL,
+  `uploaded_by_name` varchar(100) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `uploaded_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `file_path` varchar(500) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `file_size` bigint DEFAULT NULL,
+  `status` enum('Active','Archived') COLLATE utf8mb4_0900_ai_ci DEFAULT 'Active',
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=90001;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `respondent`
+--
+
+DROP TABLE IF EXISTS `respondent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `respondent` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lastname` varchar(100) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mobile` varchar(15) COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('Admin','Leadership','Respondent') COLLATE utf8mb4_0900_ai_ci DEFAULT 'Respondent',
+  `status` enum('Active','Inactive') COLLATE utf8mb4_0900_ai_ci DEFAULT 'Active',
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=210005;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-14 11:10:29
